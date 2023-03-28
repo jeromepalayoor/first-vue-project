5759c6aff8cf62cf40bce6801c510d16c42bf59c
jeromepalayoor <me@jp-ch.gq>
2023-03-28 23:15:54 +0800

    idk

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  baseUrl: process.env.NODE_ENV === 'production'
+    ? '/first-vue-project/'
+    : '/'
 })
